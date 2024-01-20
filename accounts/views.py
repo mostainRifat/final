a274dbabfe7cfb181fcf6cc733553dfa14f118d7
Mostain Rifat <120373058+mostainRifat@users.noreply.github.com>
2024-01-20 23:18:01 +0600

    Add files via upload

---
+++ accounts/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
+from django.contrib.auth import login, logout, authenticate
+
+def get_create_session(request):
+    if not request.session.session_key:
+        request.session.create()
+    return request.session.session_key
+def register(request):
+    form = RegistrationForm()
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/store')
+    return render(request, 'accounts/register.html', {'form':form})
+
+def profile(request):
+    return render(request, 'accounts/dashboard.html')
+
+def user_login(request):
+    if request.method == 'POST':
+        user_name = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username = user_name, password = password)
+        # print(user)
+        #session_key = get_create_session(request)
+        login(request, user)
+        return redirect('profile')
+    return render(request, 'accounts/signin.html')
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
